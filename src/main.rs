6d3895a3ded930bc35426af592da27bc7756fcb7
yujichino <chino@jsl.co.jp>
2022-02-23 15:28:24 +0900

    add rand 🚚

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");          // 数を当ててごらん
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);    // 秘密の数は次の通り: {}
+
+    println!("Please input your guess.");   // ほら、予想を入力してね
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");     // 行の読み込みに失敗しました
+    
+    println!("You guessed: {}", guess);     // 行のように予想しました: {}
 }
