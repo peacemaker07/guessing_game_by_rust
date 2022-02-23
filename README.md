6029a3f382ee986d79550497db7608d245c91bf1
yujichino <chino@jsl.co.jp>
2022-02-23 15:51:55 +0900

    change repeat game➰

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # guessing_game_by_rust
+
+Try [数字あてゲームをプログラムする](https://doc.rust-jp.rs/book-ja/ch02-00-guessing-game-tutorial.html) site!!!!
--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,26 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);    // 秘密の数は次の通り: {}
 
-    println!("Please input your guess.");   // ほら、予想を入力してね
+    loop {
+        println!("Please input your guess.");   // ほら、予想を入力してね
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");     // 行の読み込みに失敗しました
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");     // 行の読み込みに失敗しました
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");   // 数値を入力してください！
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");   // 数値を入力してください！
 
-    println!("You guessed: {}", guess);     // 行のように予想しました: {}
+        println!("You guessed: {}", guess);     // 行のように予想しました: {}
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),   // 小さすぎ!
-        Ordering::Greater => println!("Too big!"),  // 大きぎ!
-        Ordering::Equal => println!("You win!"),    // やったね!
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),   // 小さすぎ!
+            Ordering::Greater => println!("Too big!"),  // 大きぎ!
+            Ordering::Equal => {
+                println!("You win!");                   // やったね!
+                break;
+            }
+        }
     }
 }
